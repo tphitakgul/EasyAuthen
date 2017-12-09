1f741f502000288738b77b4a1dfc1325781cbc3e
Boss <t.phitakgul@gmail.com>
2017-12-09 14:59:13 +0700

    init easy authentic

---
+++ Easy Authen/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Easy Authen
+//
+//  Created by Apple on 12/9/2560 BE.
+//  Copyright © 2560 Boss. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
